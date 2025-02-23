a606d05ceee61a21e31b4938dc82be325b0027fa
MajdSoubh <majdsoubh53@gmail.com>
2025-02-23 19:59:23 +0300

    [FIX] Used Item as type for insert and add instead of CartItem

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -5,17 +5,21 @@ interface CartItem {
   quantity: number;
   price: number;
 }
+interface Item {
+  id: number;
+  price: number;
+}
 
 interface CartContextType {
   items: CartItem[];
-  addItem: (product: CartItem, quantity?: number) => void;
+  addItem: (item: Item, quantity?: number) => void;
   removeItem: (itemId: number) => void;
   updateItemQuantity: (
     itemId: number,
     operation: "increment" | "decrement" | "set",
     quantity: number
   ) => void;
-  insertOrUpdateItem: (item: CartItem, quantity: number) => void;
+  insertOrUpdateItem: (item: Item, quantity: number) => void;
   getItemQuantity: (itemId: number) => number;
   getTotalPrice: () => string;
   getTotalItems: () => number;
@@ -34,7 +38,7 @@ interface CartProviderProps {
 export const CartProvider = ({ children }: CartProviderProps) => {
   const [items, setItems] = useState<CartItem[]>([]);
 
-  const addItem = (item: CartItem, quantity: number = 1) => {
+  const addItem = (item: Item, quantity: number = 1) => {
     if (quantity === 0) return;
     setItems([...items, { ...item, quantity }]);
   };
@@ -77,7 +81,7 @@ export const CartProvider = ({ children }: CartProviderProps) => {
     );
   };
 
-  const insertOrUpdateItem = (item: CartItem, quantity: number) => {
+  const insertOrUpdateItem = (item: Item, quantity: number) => {
     if (quantity < 0) return;
     const existing = items.find((_item) => _item.id === item.id);
     if (existing) {
