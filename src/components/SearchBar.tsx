9c47222a5cd9251d23070108f0553ce01db98138
MajdSoubh <majdsoubh53@gmail.com>
2025-02-23 17:42:12 +0300

    [WIP] Added search bar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,14 +1,28 @@
-import { SearchIcon } from "./Icons";
+import { Search } from "lucide-react";
+import { ChangeEventHandler, FC } from "react";
 
-export default function SearchBar({ placeholder = "...", className = "" }) {
+interface SearchBarProps {
+  placeholder?: string;
+  className?: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+}
+
+const SearchBar: FC<SearchBarProps> = ({
+  placeholder = "...",
+  className = "",
+  onChange,
+}) => {
   return (
-    <div className={"relative w-full " + className}>
-      <SearchIcon className="absolute top-1/2 left-2 -translate-y-1/2" />
+    <div className={`relative w-full ${className}`}>
+      <Search className="absolute text-primary top-1/2 left-2 -translate-y-1/2" />
       <input
         type="text"
         placeholder={placeholder}
-        className="px-2 py-2 w-full rounded-lg border-2 border-primary pl-9 hover:ring-1 hover:ring-primary/50  transition-all focus:outline-0 duration-200"
+        onChange={onChange}
+        className="px-2 py-2 text-center w-full rounded border-2 border-primary pl-10 hover:ring-1 hover:ring-primary/50 bg-background-secondary transition-all focus:outline-0 duration-200"
       />
     </div>
   );
-}
+};
+
+export default SearchBar;
